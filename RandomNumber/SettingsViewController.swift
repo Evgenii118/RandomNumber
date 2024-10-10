7d3faf83db2ef9f8961bff8c367d7c7c4436aaef
Ewgeny Izyurov <help@izyapp.ru>
2024-10-10 10:05:35 +0500

    Add new SettingsViewController

---
+++ RandomNumber/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  RandomNumber
+//
+//  Created by Ewgeniy Izyurov on 10.10.2024.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
